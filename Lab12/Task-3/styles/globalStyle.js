f66a6a7b5ff26f068c9681a169f186edb3f44615
SkullFang24 <159981320+SkullFang24@users.noreply.github.com>
2024-04-16 20:39:46 +0500

    Lab12

---
+++ Lab12/Task-3/styles/globalStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles1 = StyleSheet.create({
+containerGlobal: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    padding: 8,
+},
+paragraphGlobal: {
+    marginTop: 18,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+},
+
+});
